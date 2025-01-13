2ccb77e69fa00b9ab46c6c548a63b68e88b27bd7
Aleksandr Ovsyannikov <116282471+AlexOneWRLD@users.noreply.github.com>
2025-01-13 11:59:46 +0300

    add about

--- src/js/project/index.js
+++ src/js/project/index.js
@@ -34,3 +34,15 @@ var spans = wrapper.getElementsByTagName("span");
 for (var i = 0; i < spans.length; i++) {
   spans[i].style.animationDelay = i * 30 + "ms";
 }
+
+
+
+var wrapper = document.getElementsByClassName("about__text")[0];
+wrapper.style.opacity = "1";
+wrapper.innerHTML = wrapper.textContent.replace(/./g, "<span>$&</span>");
+
+var spans = wrapper.getElementsByTagName("span");
+
+for (var i = 0; i < spans.length; i++) {
+  spans[i].style.animationDelay = i * 30 + "ms";
+}
