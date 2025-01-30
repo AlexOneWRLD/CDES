17d7b5a5cdf14cede6bba4f49bf35fc18fb27303
Anna-Anka <anjbekrenewa@gmail.com>
2025-01-30 12:05:46 +0300

    refactor tag-animation

--- src/js/project/_tag-animation-scroll.js
+++ src/js/project/_tag-animation-scroll.js
@@ -1,35 +1,20 @@
+const scrollHandler = (hero, heroDecor) => {
+    if (window.scrollY > hero.getBoundingClientRect().top + window.scrollY) {
+        heroDecor.classList.add('hero__decor--small');
+    } else {
+        heroDecor.classList.remove('hero__decor--small');
+    }
+}
+
 (function () {
     const heroDecor = document.querySelector('.hero__decor');
-    const dataBlocks = document.querySelectorAll('[data-descr]'); // Замените на ваш атрибут или класс
+    const heroText = document.querySelector('.hero__wrapper')
 
-    if (!heroDecor || !dataBlocks) {
+    if (!heroDecor || !heroText) {
         return null
     }
 
-    // Функция проверки положения блока
-    function isInViewport(element) {
-        const rect = element.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-        );
-    }
+    scrollHandler(heroText, heroDecor)
 
-    window.addEventListener('scroll', () => {
-        let inVisibleContext = false; // Флаг для проверки видимости блоков
-
-        // Проверяем положение каждого блока
-        dataBlocks.forEach(block => {
-            if (isInViewport(block)) {
-                inVisibleContext = true; // Если хотя бы один блок виден, устанавливаем флаг
-            }
-        });
-
-        // Изменяем размер при скролле
-        if (window.scrollY > 0 && !inVisibleContext) {
-            heroDecor.classList.add('hero__decor--small');
-        } else {
-            heroDecor.classList.remove('hero__decor--small');
-        }
-    });
+    window.addEventListener('scroll', () => scrollHandler(heroText, heroDecor));
 })()
